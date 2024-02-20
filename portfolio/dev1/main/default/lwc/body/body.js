a099b5a203359a981ac571fc05b3ec9413083571
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-02-20 11:07:14 -0600

    Checkpoint - 1.00

--- portfolio/dev1/main/default/lwc/body/body.js
+++ portfolio/dev1/main/default/lwc/body/body.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import isGuest from '@salesforce/user/isGuest';
 
-export default class Body extends LightningElement {}
+export default class Body extends LightningElement {
+    get message() {
+        return isGuest ? 'You are not logged in.' : 'You are logged in.';
+    }
+}
