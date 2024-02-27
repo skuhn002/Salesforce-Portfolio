318542f814415c7f08571cf912fd24ab2482b39d
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-02-27 14:02:27 -0600

    Add Login Link to Header - 1.00

--- portfolio/dev1/main/default/lwc/header/header.js
+++ portfolio/dev1/main/default/lwc/header/header.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class Header extends LightningElement {}
+export default class Header extends LightningElement {
+   handleClick() {
+       console.log('Button clicked');
+   }
+}
