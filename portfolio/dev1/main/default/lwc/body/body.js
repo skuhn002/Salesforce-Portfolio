aa66b69f0a35f804c6303f19896f8b00eb13dace
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-10-20 04:13:33 -0500

    Update Site to Use Links - 1.00

--- portfolio/dev1/main/default/lwc/body/body.js
+++ portfolio/dev1/main/default/lwc/body/body.js
@@ -1,7 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import isGuest from '@salesforce/user/isGuest';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import NAVIGATION_MESSAGE_CHANNEL from '@salesforce/messageChannel/NavigationMessageChannel__c';
+
 
 export default class Body extends LightningElement {
+    page = 'home';
+
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext,
+            NAVIGATION_MESSAGE_CHANNEL,
+            (message) => this.handleMessage(message)
+        );
+    }
+
+    handleMessage(message) {
+        this.page = message.page;
+        console.log('Page value received:', this.page);
+    }
+
+    get isHomePage() {
+        return this.page === 'home';
+    }
+
+    get isResumePage() {
+        return this.page === 'resume';
+    }
+
+    get isS2TPage() {
+        return this.page === 's2t';
+    }
+
     get message() {
         return isGuest ? 'NOT LOGGED IN' : 'LOGGED IN';
     }
--- portfolio/dev1/main/default/lwc/header/header.js
+++ portfolio/dev1/main/default/lwc/header/header.js
@@ -1,7 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import NAVIGATION_MESSAGE_CHANNEL from '@salesforce/messageChannel/NavigationMessageChannel__c';
 
 export default class Header extends LightningElement {
-   handleClick() {
-       console.log('Button clicked');
+    @wire(MessageContext)
+    messageContext;
+
+    page = 'home';
+
+    navigate(page) {
+        const payload = { page };
+        publish(this.messageContext, NAVIGATION_MESSAGE_CHANNEL, payload);
+    }
+
+    navToHome() {
+        this.page = 'home';
+        this.navigate(this.page);
+    }
+
+   navToResume() {
+        this.page = 'resume';
+        this.navigate(this.page);
+   }
+
+   navToS2T() {
+        this.page = 's2t';
+        this.navigate(this.page);
    }
 }
