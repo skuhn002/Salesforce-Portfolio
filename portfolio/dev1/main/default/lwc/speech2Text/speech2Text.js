cd8880d868f2f2477ccb6af0816597f1c59407ae
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-10-20 03:20:12 -0500

    Speech to Text Works :) - 1.00

--- portfolio/dev1/main/default/lwc/speech2Text/speech2Text.js
+++ portfolio/dev1/main/default/lwc/speech2Text/speech2Text.js
@@ -1,3 +1,48 @@
 import { LightningElement } from 'lwc';
 
-export default class Speech2Text extends LightningElement {}
+export default class Speech2Text extends LightningElement {
+    recordingState = false;
+    recordingStateWas = false;
+
+    transcript = '';
+
+    recognition = new webkitSpeechRecognition();
+    
+    connectedCallback() {
+        this.recognition.continuous = true;
+        this.recognition.interimResults = true;
+
+        this.recognition.onresult = (event) => {
+            console.log('Made it to onresult');
+            for (let i = event.resultIndex; i < event.results.length; ++i) {
+                if (event.results[i].isFinal) {
+                    this.transcript += event.results[i][0].transcript;
+                }
+            }
+        };
+    }
+
+
+    toggleRecording() {
+        this.recordingStateWas = this.recordingState;
+        this.recordingState = !this.recordingState;
+
+
+        if (this.recordingStateWas === false && this.recordingState === true) {
+            //Start Recording
+            this.recognition.start();
+
+            console.log('Recording Started');
+        }
+        if (this.recordingStateWas === true && this.recordingState === false) {
+            //Stop Recording
+            console.log('Recording Stopped');
+            this.recognition.stop();
+        }
+    }
+
+    clearTranscript() {
+        this.transcript = '';
+        console.log('Transcript cleared');
+    }
+}
